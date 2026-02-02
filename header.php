7db9d19a7200701c815ab3921c0423b7b50ff8fb
Carlos Santos <cmps.5@hotmail.com>
2026-02-02 20:57:43 +0000

    add header

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+
+echo '
+    <!DOCTYPE html>
+    <html lang="pt">
+        <head>
+            <meta charset="UTF-8">
+            <title>Micro Forum</title>
+            <link rel="stylesheet" href="css/main.css">
+        </head>
+        <body>
+            <div class ="forum_container">
+                <img src="img/logo.png" alt="Micro Forum Logo">
+                <hr>
+';
+
+?>
