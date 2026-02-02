dbf754c4f91b7d9f07a8c673457c4a19981473a5
Carlos Santos <cmps.5@hotmail.com>
2026-02-02 20:08:42 +0000

    add database and users table creation in installation script

--- install.php
+++ install.php
@@ -6,11 +6,30 @@ include 'config.php';
 $connection = new PDO("mysql:host=$host", $user, $db_password);
 $engine = $connection->prepare(
     "CREATE DATABASE IF NOT EXISTS $data_base
-    DEFAULT CHARACTER SET utf8mb4"
+    DEFAULT CHARACTER SET utf8"
 );
 $engine->execute();
 $connection = null;
 
 echo '<p>Database created successfully</p><hr>';
+
+/**
+ * Create tables
+ */
+
+// users table
+$connection = new PDO("mysql:host=$host;dbname=$data_base;charset=utf8", $user, $db_password);
+
+$sql = "CREATE TABLE IF NOT EXISTS users (
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    avatar VARCHAR(255)
+)";
+
+$engine = $connection->prepare($sql);
+$engine->execute();
+
+echo "<p>Table 'users' created successfully</p><hr>";
 
 ?>
