40b8ff543e3c09f13142840a7450c814cd9576a3
Carlos Santos <cmps.5@hotmail.com>
2026-02-02 21:45:36 +0000

    add footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+
+echo '</div>'; // close forum_container
+
+echo '
+            <footer class="footer">
+                Webmaster &copy; 2026
+            </footer>
+            </div>
+        </body>
+    </html>
+';
+
+?>
